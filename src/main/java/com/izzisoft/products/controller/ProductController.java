f97e6e3f028c29ca95d4e174b16946bdab4c7986
Karlz-Bandz <karol2646@gmail.com>
2026-03-18 10:57:14 +0100

    Spring security config

--- src/main/java/com/izzisoft/products/controller/ProductController.java
+++ src/main/java/com/izzisoft/products/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.izzisoft.products.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,18 +25,21 @@ public class ProductController {
     private final ProductService productService;
 
     @PutMapping("/decrease/{id}/{quantity}")
+    @PreAuthorize("hasRole('SERVICE')")
     public ResponseEntity<Void> decreaseProductQuantity(@PathVariable("id") Long id, @PathVariable("quantity") int quantity) {
         productService.decreaseProductQuantity(id, quantity);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> updateProduct(@PathVariable("id") Long id, @RequestBody ProductDto productDto) {
         productService.updateProduct(id, productDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteProductById(@PathVariable("id") Long id) {
         productService.deleteProductById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -47,6 +51,7 @@ public class ProductController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> addNewProduct(@RequestBody ProductDto productDto) {
         productService.addNewProduct(productDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
